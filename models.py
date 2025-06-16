314e199183c10cd909bc602c74983d4ce6909c97
emily00106 <emily00106@gmail.com>
2025-06-16 21:36:04 +0800

    Update models.py

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
     notes = db.relationship('Note', backref='user', lazy=True)
 
     def __repr__(self):
